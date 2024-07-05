f629bac4975e426ad942f127dbed6b84a605f9a3
ankit <ankit21014@iiitd.ac.in>
2024-07-06 00:28:38 +0530

    prod code

--- YT-frontend/src/axios.js
+++ YT-frontend/src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1', // Replace with your API base URL
+  // baseURL: 'http://127.0.0.1:8000/api/v1', // Replace with your API base URL
+  baseURL: "https://vidflare2.onrender.com/api/v1", // production
   withCredentials: true
 });
 
